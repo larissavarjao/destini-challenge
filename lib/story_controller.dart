e4b1c0e251ddf9eec9f4e6caa0870285bd0c2bcd
Larissa Varjão <sanar@MacBook-Pro-de-Sanar.local>
2020-05-20 00:09:11 -0300

    feat: final adjustment on button visibility

--- lib/story_controller.dart
+++ lib/story_controller.dart
@@ -62,6 +62,8 @@ class StoryController {
   void reset() {
     _currentStory = 0;
   }
-}
 
-//TODO: Step 27 - Create a method called buttonShouldBeVisible() which checks to see if storyNumber is 0 or 1 or 2 (when both buttons should show choices) and return true if that is the case, else it should return false.
+  bool buttonShouldBeVisible() {
+    return (_currentStory == 0 || _currentStory == 1 || _currentStory == 2);
+  }
+}
